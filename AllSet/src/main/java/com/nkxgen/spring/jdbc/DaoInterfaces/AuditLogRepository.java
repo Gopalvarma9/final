7f7fa698f30384411667923be5fec80e50fba33b
valli369 <valli369@Hijacker>
2023-07-02 13:40:53 +0530

    commit

---
+++ AllSet/src/main/java/com/nkxgen/spring/jdbc/DaoInterfaces/AuditLogRepository.java
@@ -0,0 +1,18 @@
+package com.nkxgen.spring.jdbc.DaoInterfaces;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nkxgen.spring.jdbc.model.AuditLogs;
+
+public interface AuditLogRepository extends JpaRepository<AuditLogs, Long> {
+
+	Page<AuditLogs> findAllByOrderByTimestampDesc(Pageable pageable);
+
+	Page<AuditLogs> findAll(Pageable pageable);
+
+	List<AuditLogs> findAll();
+}
